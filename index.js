8a20d738f0bfc119a12338e507c029d276d58199
xLegitGhost <uriel@Uriels-MacBook-Air.local>
2026-03-14 14:20:55 -0600

    Added findById and findByName for pokemon endpoint

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 import express from 'express';
+import pokedex from './pokedex.json' with { type: 'json' };
 
 const app = express();
+const { pokemon } = pokedex; 
 
 app.get('/', (req, res) => {
-    res.send('Hola mundo desde express');
+    return res.send('Welcome to the Pokemon API').status(200);
 });
+
+app.get('/pokemon/all', (req, res) => {
+    return res.send(pokemon).status(200);
+});
+
+app.get(/^\/pokemon\/([0-9]{1,3})$/, (req, res) => {
+    const id = req.params[0] - 1;
+    if ( id >= 0 && id <= 151) {
+        return res.send(pokemon[id]).status(200);
+    }else{
+        return res.send('Not found').status(404);
+    }
+})
+
+app.get('/pokemon/:name', (req, res) => {
+  const name = req.params.name;
+
+    const pokemonData = pokemon.find(p => p.name.toLowerCase() === name.toLowerCase());
+    if(!pokemonData) return res.status(404).send('Not found');
+
+    return res.send(pokemonData).status(200);
+  
+});   
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
