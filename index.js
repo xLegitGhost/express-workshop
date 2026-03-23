595d33e826829787dcae4050ec6ab0e31b38657b
xLegitGhost <contacto.urielramirez@gmail.com>
2026-03-22 18:53:20 -0600

    Add Morgan & Routes Folder

--- index.js
+++ index.js
@@ -1,38 +1,18 @@
 import express from 'express';
-import pokedex from './pokedex.json' with { type: 'json' };
+import morgan from "morgan";
+import { pokemonRoute } from './routes/pokemon.js';
 
 const app = express();
-const { pokemon } = pokedex; 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 app.get('/', (req, res) => {
     return res.send('Welcome to the Pokemon API').status(200);
-});
+});  
 
-app.get('/pokemon', (req, res) => {
-    return res.send(pokemon).status(200);
-});
-
-app.get(/^\/pokemon\/([0-9]{1,3})$/, (req, res) => {
-    const id = req.params[0] - 1;
-    if ( id >= 0 && id <= 151) {
-        return res.send(pokemon[id]).status(200);
-    }else{
-        return res.send('Not found').status(404);
-    }
-})
-
-app.get('/pokemon/:name', (req, res) => {
-  const name = req.params.name;
-
-    const pokemonData = pokemon.find(p => p.name.toLowerCase() === name.toLowerCase());
-    if(!pokemonData) return res.status(404).send('Not found');
-
-    return res.send(pokemonData).status(200);
-  
-});   
+app.use("/pokemon", pokemonRoute)
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
---
+++ routes/pokemon.js
@@ -0,0 +1,32 @@
+import express from "express"
+import pokedex from '../pokedex.json' with { type: 'json' };
+const pokedexJson = pokedex.pokemon; 
+
+export const pokemonRoute = express.Router();
+
+pokemonRoute.post("/", (req, res) => {
+    return res.status(200).send(req.body);
+})
+
+pokemonRoute.get('/', (req, res) => {
+  return res.send(pokedexJson).status(200);
+});
+
+pokemonRoute.get(/^\/pokemon\/([0-9]{1,3})$/, (req, res) => {
+    const id = req.params[0] - 1;
+    if ( id >= 0 && id <= 151) {
+        return res.send(pokedexJson[id]).status(200);
+    }else{
+        return res.send('Not found').status(404);
+    }
+})
+
+pokemonRoute.get('/:name', (req, res) => {
+  const name = req.params.name;
+
+    const pokemonData = pokedexJson.find(p => p.name.toLowerCase() === name.toLowerCase());
+    if(!pokemonData) return res.status(404).send('Not found');
+
+    return res.send(pokemonData).status(200);
+  
+}); 
