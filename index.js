423b8d4575f13ce184907478a5f0b7e625f71a13
xLegitGhost <contacto.urielramirez@gmail.com>
2026-04-13 13:08:12 -0600

    Refactor Pokémon routes to standardize JSON response format and improve error handling

--- index.js
+++ index.js
@@ -8,11 +8,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-    return res.send('Welcome to the Pokemon API').status(200);
-});  
+app.get('/', (req, res, next) => {
+    return res.status(200).json({
+        code: 200, 
+        message: "Bienvenido al Pokedex"
+    });
+});
 
 app.use("/pokemon", pokemonRoute)
+
+app.use((req, res, next) => {
+    return res.status(404).json({
+        code: 404,
+        message: "URL no encontrada"
+    });
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -9,16 +9,16 @@ pokemonRoute.post("/", (req, res) => {
 
 pokemonRoute.get('/', async (req, res) => {
     const [rows] = await pool.query('SELECT * FROM pokemon');
-    return res.status(200).json(rows);
+    return res.status(200).json({code: 200, data: rows});
 });
 
 pokemonRoute.get(/^\/pokemon\/([0-9]{1,3})$/, async (req, res) => {
     const id = req.params[0] - 1;
     if ( id >= 0 && id <= 151) {
         const [rows] = await pool.query('SELECT * FROM pokemon WHERE id = ?', [id + 1]);
-        return res.status(200).json(rows[0]);
+        return res.status(200).json({code: 200, data: rows[0]});
     }else{
-        return res.status(404).json({ error: 'Not found' });
+        return res.status(404).json({code: 404, error: 'Not found' });
     }
 })
 
@@ -26,8 +26,8 @@ pokemonRoute.get('/:name', async (req, res) => {
   const name = req.params.name;
 
     const queryResult = await pool.query('SELECT * FROM pokemon WHERE name = ?', [name]);
-    if(!queryResult[0]) return res.status(404).json({ error: 'Not found' });
+    if(!queryResult[0]) return res.status(404).json({code: 404, error: 'Not found' });
 
-    return res.status(200).json(queryResult[0]);
+    return res.status(200).json({code: 200, data: queryResult[0]});
   
 }); 
