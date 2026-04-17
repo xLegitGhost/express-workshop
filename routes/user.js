6d1361d86c19c77f048cfbbb83f0d38189195456
xLegitGhost <contacto.urielramirez@gmail.com>
2026-04-16 20:20:35 -0600

    Add JWT authentication to user login route and update dependencies

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import db from '../config/database.js';
+import jwt from 'jsonwebtoken';
 
 export const userRoute = express.Router();
 
@@ -20,4 +21,25 @@ userRoute.post("/", async (req, res) => {
     if (result.affectedRows === 0) return res.status(500).json({ code: 500, error: "Error inserting user" });
     
     return res.status(201).json({ code: 201, message: 'User created successfully' }); 
+});
+
+userRoute.post("/login", async (req, res) => {
+    const { user_mail, user_password } = req.body;
+    
+    if (!user_mail || !user_password) return res.status(400).json({ code: 400, error: "Bad request, missing required fields" });
+
+    const query = "SELECT * FROM user WHERE user_mail = ? AND user_password = ?";
+    const [rows] = await db.query(query, [user_mail, user_password]);
+
+    if (rows.length === 0) {
+        return res.status(401).json({ code: 401, error: "Invalid email or password" });
+    }
+
+    const token = jwt.sign(
+        { user_id: rows[0].user_id, user_mail: rows[0].user_mail },
+        "debugKey",
+        { expiresIn: "1h" }
+    )
+
+    return res.status(200).json({ code: 200, message: "Login successful", token });
 });
